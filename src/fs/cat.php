1b2d24843baddc7dc062868696286d9879e6fad1
whysosergious <sergio.stankevich@gmail.com>
2021-02-17 04:31:44 +0100

    implemented component editing

--- src/fs/cat.php
+++ src/fs/cat.php
@@ -14,7 +14,18 @@
   $list_length = count( $file_list );   // total items in queue
   
   // init message
-  echo $response = "Start processing files with  =>  '$ground_zero_file'. \n";
+  // function console_log($output, $with_script_tags = true) {
+  //   $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
+  // ');';
+  //     if ($with_script_tags) {
+  //         $js_code = '<script>' . $js_code . '</script>';
+  //     }
+  //     return $js_code;
+  // }
+  
+  $log[] = "Started processing files with  =>  '$ground_zero_file'";
+
+  // echo "Started processing files with  =>  '$ground_zero_file'. \n";
 
   // create ZCM directory if none exists
   if(!is_dir("../$zcm_dir")) {
@@ -27,10 +38,13 @@
   $current_file = $ground_zero_file;
   $count = 0;
 
+  
+
   // main copycat function
   function _cat( $file, $path, $dir, $count, $file_list, $list_length ) {
-    
-    echo ++$count . "/" . $list_length . " || ";  // iterate and display progress
+    global $log;
+    // ++$count;
+    $prep_count = ++$count . "/" . $list_length . " || " ;  // iterate and display progress
     
     // create path directory for current file
     if ( $path != '' && !is_dir("../" . $dir . "/" . $path) ) {
@@ -40,8 +54,9 @@
     // extension description : z-pre-proccessed
     if ( copy("../$path$file", "../$dir/$path$file.zprep") ) {
 
-      echo "Cat copied '$file' to |[ 'src/$dir/$path' ]|. \n";  // task msg
+      $log[] = $prep_count . "Cat copied '$file' to |[ 'src/$dir/$path' ]|.";  // task msg
 
+      _prepComponent( $count, $file, $file_list[$count-1]['path'], file_get_contents("../$dir/$path$file.zprep") );
       // check if last
       if ( $count < $list_length ) {
 
@@ -51,13 +66,34 @@
         _cat( $file, $path, $dir, $count, $file_list, $list_length );   // next
       } else {
 
-        echo "**** Cat done! ^-^ ****";   // copycat finished msg
+        $log[] = "**** Cat done! ^-^ ****";   // copycat finished msg
+        _respond();
       }
     } else { 
 
-      // error message, details given in the warning on the previous line
-      echo "**!!  Cat failed to copy '$file', check file path and name. If one already exists in 'src/$dir/$path' it needs be rewriteble  !!**. \n";
+      // else =)
+      $log[] = "**!!  Cat failed to copy '$file', check file path and name. If one already exists in 'src/$dir/$path' it needs be rewriteble  !!**.";
     }
   } _cat( $current_file, $current_path, $zcm_dir, $count, $file_list, $list_length ); // init run
 
+  $response = array();
+
+
+
+  function _prepComponent( $count, $file, $path, $code ) {
+    global $response;
+
+    $response[ ] = (object) [ "data" => [
+      "name" => $file, 
+      "path" => $path, 
+      "code" => $code,
+      "queue" => $count ]
+    ];
+  }
+
+  function _respond() {
+    global $response, $log;
+
+    echo json_encode([ "log" => $log, "content" => $response ]);
+  }
 ?>
